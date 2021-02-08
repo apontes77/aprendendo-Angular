7c7f887f13b720e3c24c7740cf8c3a8aca2115a0
apontes77 <alexandrepontesgyn@gmail.com>
2021-02-08 09:14:16 -0300

    fix: reordering versioning of folders

---
+++ frontend/src/app/views/products-crud/products-crud.component.ts
@@ -0,0 +1,28 @@
+import { HeaderService } from './../../components/template/header/header.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-products-crud',
+  templateUrl: './products-crud.component.html',
+  styleUrls: ['./products-crud.component.css']
+})
+export class ProductsCrudComponent implements OnInit {
+
+  constructor(private router: Router, private headerService: HeaderService) { 
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routerUrl: '/products'
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  navigateToProductCreate() : void {
+    this.router.navigate(['/products/create'])
+    console.log('navegando')
+  }
+
+}
